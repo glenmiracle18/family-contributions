f0bb01364fc2aa9f062466d8b0745f2f3f788439
glenmiracle18 <m.bonyu@alustudent.com>
2024-05-30 08:47:30 +0200

    updates: last push before new branch for qucikFix

---
+++ app/components/widgets/Notification-dropdown.tsx
@@ -0,0 +1,22 @@
+import { Dropdown } from "flowbite-react";
+import { NotificationIcon } from "../svgs/notification-icon";
+
+export const NotificationDropDown = () => {
+  return (
+    <div className="flex md:order-2">
+      <Dropdown arrowIcon={false} inline label={<NotificationIcon />}>
+        <Dropdown.Header>
+          <span className="block text-sm">Glen Miracle</span>
+          <span className="block truncate text-sm font-medium">
+            name@gmail.com
+          </span>
+        </Dropdown.Header>
+        <Dropdown.Item>Dashboard</Dropdown.Item>
+        <Dropdown.Item>Settings</Dropdown.Item>
+        <Dropdown.Item>Earnings</Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Item>Sign out</Dropdown.Item>
+      </Dropdown>
+    </div>
+  );
+};
